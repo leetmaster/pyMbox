17e7177b000bbf86b0c8d5c29594ae402e04d7da
Ruben Garcia <leetmaster@users.noreply.github.com>
2019-01-24 11:05:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ import pysftp
 #USER = "LFSGB_SUPPORT"
 #PASSWORD = "Lf5jde18!"
 
-#HOST = "jruben.ga"
-#USER = "eztigma"
-#PASSWORD = "Z0ge0057"
+HOST = "jruben.ga"
+USER = "eztigma"
+PASSWORD = "Z0ge0057"
 
 # Override hostkey although it will still send a warning
 cnopts = pysftp.CnOpts()
